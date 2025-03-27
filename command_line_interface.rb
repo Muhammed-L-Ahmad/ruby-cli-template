4a8692b3db41d3a898661131ce57dad18fd8bcd4
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-27 17:22:14 +0000

    more methods

--- command_line_interface.rb
+++ command_line_interface.rb
@@ -10,8 +10,20 @@ if their_operation == "addition"
   puts @sum(numbers)
 end
 
-if their_operation == "subtraction"
+elsif their_operation == "subtraction"
   pp "what two numbers would you like to subtract from one another?"
   numbers(a, b) = gets.chomp.to_i
   puts @difference(numbers)
 end
+
+elsif their_operation == "multiplication"
+  pp "what two numbers would you like to multiply?"
+  numbers(a, b) = gets.chomp.to_i
+  puts @product(numbers)
+  end
+
+else
+  pp "what numbers do you want to divide by one another?"
+    numbers(a, b) = gets.chomp.to_i
+    puts @dividend(numbers)
+  end
